1eb612c101f739cf28ce3f1258ed6887884cc30d
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-24 17:53:50 +0330

    create static methods clearScreen and handleErrors

--- src/main/java/SBU/CS/Main.java
+++ src/main/java/SBU/CS/Main.java
@@ -1,8 +1,43 @@
 package SBU.CS;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+    }
 
+    public static void clearScreen() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
+    public static int handleErrors(int firstValue, int lastValue) { // for handling exceptions in getting input
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.printf("Enter an option (from %d to %d)", firstValue, lastValue);
+            String input = scanner.nextLine();
+
+            try {
+                number = Integer.parseInt(input);
+                // Check if the input is a valid option
+                if (number >= firstValue && number <= lastValue) {
+                    validInput = true;
+                } else {
+                    System.out.printf("Enter an option (from %d to %d)\n", firstValue, lastValue);
+                }
+            } catch (NumberFormatException e) {
+                // Handle the case where input is not a valid integer
+                System.out.println("Invalid input: Please enter a valid integer.");
+            }
+        }
+        // Close the scanner
+        scanner.close();
+
+        return number;
     }
 }
