a466c53c318bc9a487ada9c8981d39c8d73da545
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-25 13:54:53 +0330

    created Tools.java for general terminal uses

---
+++ src/main/java/SBU/CS/Tools.java
@@ -0,0 +1,52 @@
+package SBU.CS;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Tools {
+
+    public static void clearScreen() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
+    public static int handleErrors(int firstValue, int lastValue) { // for handling exceptions in getting input
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.printf("Enter an option (from %d to %d)", firstValue, lastValue);
+            String input = scanner.nextLine();
+
+            try {
+                number = Integer.parseInt(input);
+                // Check if the input is a valid option
+                if (number >= firstValue && number <= lastValue) {
+                    validInput = true;
+                } else {
+                    System.out.printf("Enter an option (from %d to %d)\n", firstValue, lastValue);
+                }
+            } catch (NumberFormatException e) {
+                // Handle the case where input is not a valid integer
+                System.out.println("Invalid input: Please enter a valid integer.");
+            }
+        }
+        // Close the scanner
+        scanner.close();
+
+        return number;
+    }
+
+    public static boolean validateEmailFormat(String email) {
+        String regex = ".+@.+\\..+";
+
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.find();
+    }
+}
