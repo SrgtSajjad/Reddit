6909dc3bf904ae25a80c880a842c5488dfb7c090
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-26 23:54:33 +0330

    Created Database.java

---
+++ src/main/java/SBU/CS/Database.java
@@ -0,0 +1,11 @@
+package SBU.CS;
+
+import SBU.CS.Account.User;
+import SBU.CS.Subreddit.Subreddit;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Subreddit> subreddits = new ArrayList<>();
+}
