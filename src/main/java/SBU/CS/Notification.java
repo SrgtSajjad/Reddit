fa983a0284eef444ba2781b9bb9ecd81b2d51439
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-29 12:07:40 +0330

    Notification class created

---
+++ src/main/java/SBU/CS/Notification.java
@@ -0,0 +1,13 @@
+package SBU.CS;
+
+public class Notification {
+
+    String title;
+    String text;
+
+
+    public Notification(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+}
