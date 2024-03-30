067fa4b88b66d42779507ce22b53fed2bb3e9b8b
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-30 15:51:04 +0330

    updated AccountManagement interface

--- src/main/java/SBU/CS/Account/Account.java
+++ src/main/java/SBU/CS/Account/Account.java
@@ -9,13 +9,15 @@ import java.util.UUID;
 public class Account implements AccountManagement {
     private String username;
     private String password;
+    private String email;
     private final UUID accountID;
     private String firstName;
     private String lastName;
     private Birthday birthday;
     private LocalDateTime timeCreated;
-    public Account(String username, String password, String firstName, String lastName, Birthday birthday) {
+    public Account(String username, String password, String firstName, String lastName, Birthday birthday, String email) {
         this.username = username;
+        this.email = email;
         this.password = DigestUtils.sha256Hex(password);
         this.firstName = firstName;
         this.lastName = lastName;
@@ -32,11 +34,14 @@ public class Account implements AccountManagement {
     @Override
     public void changeUsername(String newUsername) {
         this.username = newUsername;
+        System.out.println("Username changed successfully");
     }
 
     @Override
     public void changePassword(String newPassword) {
         this.password = DigestUtils.sha256Hex(newPassword);
+        System.out.println("Password changed successfully");
+
     }
 
     public String getUsername() {
@@ -51,33 +56,50 @@ public class Account implements AccountManagement {
         return firstName;
     }
 
+    @Override
     public void changeFirstName(String newFirstName) {
         this.firstName = newFirstName;
+        System.out.println("First name changed successfully");
+
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    @Override
     public void changeLastName(String newLastName) {
         this.lastName = newLastName;
+        System.out.println("Last name changed successfully");
+
     }
 
     public Birthday getBirthday() {
         return birthday;
     }
 
+    @Override
     public void changeBirthday(Birthday newBirthday) {
         if (Birthday.validateBirthday(newBirthday.year, newBirthday.month, newBirthday.day)) {
             this.birthday = newBirthday;
             System.out.println("Birthday changed successfully");
         }
         else
-            System.out.println("Entered date is not available, please try again");
+            System.out.println("Invalid input: Entered date is not available");
 
     }
 
 
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void changeEmail(String newEmail) {
+        this.email = newEmail;
+        System.out.println("Email changed successfully");
+
+    }
 
     public int getAge() {
         return LocalDateTime.now().getYear() - birthday.year;
--- src/main/java/SBU/CS/Account/AccountManagement.java
+++ src/main/java/SBU/CS/Account/AccountManagement.java
@@ -4,5 +4,9 @@ public interface AccountManagement {
     public boolean validatePassword(String enteredPassword);
     public void changeUsername(String newUsername);
     public void changePassword(String newPassword);
+    public void changeFirstName(String newFirstName);
+    public void changeLastName(String newLastName);
+    public void changeBirthday(Birthday newBirthday);
+    public void changeEmail(String newEmail);
 }
 
